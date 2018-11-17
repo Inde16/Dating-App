327554704f4b3cd9a449e25a083002528c88e53b
Inde16 <sotinde@gmail.com>
2018-11-17 13:19:54 +0200

    Commit

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -100,4 +100,20 @@ getMessages(id: number , page? , itemsPerPage? , messageContainer?) {
     );
 }
 
+getMesssageThread(id: number , recipientId: number) {
+  return this.http.get<Message[]>(this.baseUrl + 'users/' + id + '/messages/thread/' + recipientId);
+}
+
+sendMessage(id: number , message: Message) {
+  return this.http.post(this.baseUrl + 'users/' + id + '/messages' , message);
+}
+
+deleteMessage(id: number , userId: number) {
+  return this.http.post(this.baseUrl + 'users/' + userId + '/messages/' + id , {});
+}
+
+markAsRead(userId: number , messageId: number) {
+  this.http.post(this.baseUrl + 'users/' + userId + '/messages/' + messageId + '/read', {}).subscribe();
+}
+
 }
